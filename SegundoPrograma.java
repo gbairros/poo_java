fff97708dad83ee5b6daeff68c87ac05464dd4ae
Gildo <gbairros@gmail.com>
2022-08-01 12:42:36 -0300

    First Class

---
+++ SegundoPrograma.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class SegundoPrograma {
+
+public static void main(String[] args) throws Exception{
+	System.out.printf("Digite um inteiro: ");
+	Scanner teclado = new Scanner(System.in);
+	int k = teclado.nextInt();
+	System.out.printf("Numero lido: %d\n", k);
+	}
+}
